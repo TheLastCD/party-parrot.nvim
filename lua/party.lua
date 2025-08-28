3106d2277fa4ade5c492968885604d59830c96a4
Charlie <34370996+TheLastCD@users.noreply.github.com>
2025-08-28 10:23:16 +0100

    Use Terminal instead of static buffer (#2)

--- lua/party.lua
+++ lua/party.lua
@@ -1,16 +1,9 @@
 local function party_buffer()
-  -- Create a new scratch buffer (unlisted, scratch)
-  local buf = vim.api.nvim_create_buf(false, true)
-  -- Set buffer name
-  vim.api.nvim_buf_set_name(buf, "*party*")
-  -- Run curl command to parrot.live and capture output
-  local output = vim.fn.systemlist("curl https://parrot.live")
-  -- Set output in buffer lines
-  vim.api.nvim_buf_set_lines(buf, 0, -1, true, output)
-  -- Open buffer in current window
-  vim.api.nvim_set_current_buf(buf)
-  -- Map 'q' to close this buffer
-  vim.api.nvim_buf_set_keymap(buf, "n", "q", ":bd!<CR>", { nowait = true, noremap = true, silent = true })
+  vim.cmd('terminal curl https://parrot.live')
+
+  local buf = vim.api.nvim_get_current_buf()
+
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'q', ':bd!<CR>', { nowait = true, noremap = true, silent = true })
 end
 
 vim.api.nvim_create_user_command("Party", party_buffer, {})
