7a44534f96f440328fc8cb55d357e57e577468e3
Charlie Elliot <charlie.elliot@connectorsubsea.com>
2025-08-28 11:09:26 +0100

    set parrot to floating window and tuned the size

--- lua/party.lua
+++ lua/party.lua
@@ -1,4 +1,19 @@
 local function party_buffer()
+  local width = math.floor(vim.o.columns * 0.265)
+  local height = math.floor(vim.o.lines * 0.42)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  vim.api.nvim_open_win(buf, true, {
+    relative = 'editor',
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = 'minimal',
+    border = 'rounded',
+  })
   vim.cmd('terminal curl https://parrot.live')
 
   local buf = vim.api.nvim_get_current_buf()
@@ -7,3 +22,5 @@ local function party_buffer()
 end
 
 vim.api.nvim_create_user_command("Party", party_buffer, {})
+
+
